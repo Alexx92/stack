4b52a2766d3ec60d793e58a2e15aec6322d5d423
Hernan <alexx92.04@gmail.com>
2016-05-04 11:55:39 -0300

    Commit de validar pila vacia

--- src/test/java/cl/ubb/stack/StackTest.java
+++ src/test/java/cl/ubb/stack/StackTest.java
@@ -2,13 +2,22 @@ package cl.ubb.stack;
 
 import static org.junit.Assert.*;
 
+import java.util.Stack;
+
 import org.junit.Test;
 
 public class StackTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void creaPilaVacia() {
+		/*Arrange*/
+		Pila pila = new Pila();
+		/*Act*/
+		boolean valor= pila.isEmpty();
+		/*Assert*/
+		assertEquals(true,valor);
+		
 	}
+
 
 }
