b1e9b5dde5a0eeac14e92fd39f76e2f10b445e68
Hernan <alexx92.04@gmail.com>
2016-05-04 12:15:02 -0300

    Agrega. class

---
+++ src/main/java/cl/ubb/stack/Pila.java
@@ -0,0 +1,21 @@
+package cl.ubb.stack;
+
+public class Pila {
+	boolean vacia;
+	public Pila(){
+		vacia=true;
+	}
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		if(vacia)
+			return true;
+		
+			return false;
+	}
+
+	public void push(int i) {
+		// TODO Auto-generated method stub
+		vacia=false;
+	}
+
+}
